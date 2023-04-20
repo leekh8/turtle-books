5f3f8fae3ca372f8ff999be7e18187e8e0328037
KOJIYEON <kamjoo990406@gmail.com>
2023-04-20 17:43:58 +0900

    rename:단순파일위칫수정

--- back-end/app.js
+++ back-end/app.js
@@ -2,12 +2,21 @@
 import cors from 'cors';
 import express from 'express';
 import { categoryRouter, productRouter } from "./routes";
+import { errorHandler } from './middlewares';
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(cors());
+app.use(cookieParser());
 
 app,use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/product", productRouter);
 app.use("/category", categoryRouter);
+
+app.use(errorHandler);
+
+app.listen(3000, () => {
+  console.log(`정상적으로 서버를 시작하였습니다.`);
+})
--- back-end/routes/user/index.js
+++ back-end/routes/user/index.js
@@ -1,2 +1,2 @@
 export * from "./products/product-router";
-export * from "./category/category-router";
+export * from ""
