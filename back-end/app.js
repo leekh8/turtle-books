6c8aa5a5a62a45d1168aed6f53b9e3386c689d94
leekh <amysun125@gmail.com>
2023-04-20 18:53:16 +0900

    Update app.js

--- back-end/app.js
+++ back-end/app.js
@@ -8,6 +8,7 @@ const cors = require("cors");
 
 // 라우터 연결
 import indexRouter from "./routes";
+import viewsRouter from "./routes/views-router";
 import usersRouter from "./routes/user/myroom/my"; // user(사용자) router 연결
 
 import { errorHandler } from "./middlewares";
@@ -15,14 +16,21 @@ import { errorHandler } from "./middlewares";
 const app = express();
 
 app.use(cors()); // cors 에러 방지
+
 app.use(express.json()); // content-type: application/json 형태
 app.use(express.urlencoded({ extended: false })); // content-type: application/x-www-form-urlencoded 형태
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+/*
+// html, css, js 라우팅
+app.use(viewsRouter);
+*/
 // api 라우터
+/*
 app.use("/", indexRouter);
+*/
 app.use("/user", usersRouter);
 
 // catch 404 and forward to error handler
@@ -33,4 +41,4 @@ app.use((req, res, next) => {
 // error handler
 app.use(errorHandler);
 
-module.exports = app;
+module.export = app;
