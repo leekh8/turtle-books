db6c3ba9160e8dfd42a1d2f3434860155ea1c4de
JoSooha <dlrhdcjs@naver.com>
2023-04-20 15:01:36 +0900

    refactor: header 레이아웃 수정

---
+++ front-end/views/item/Itemlist/index.js
@@ -0,0 +1,7 @@
+// import { headerNav } from "../../common/Header/header.html"
+// /* nav Template */
+// function addNav() {
+// 	const header = document.querySelector('.headerNav');
+// 	header.innerHTML = navTemplate();
+// }
+// addNav();
