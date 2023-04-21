c718f803f5c72a9e28a0a79ad695945d1e3416ed
KOJIYEON <kamjoo990406@gmail.com>
2023-04-21 17:59:32 +0900

    feat:product,category-schema추가

---
+++ back-end/db/schemas/product-schema.js
@@ -0,0 +1,53 @@
+const { Schema } = require('mongoose');
+
+const productSchema = new Schema (
+  {
+    title: { // 제목
+      type: String,
+      required: true,
+    },
+    author: { // 작가명
+      type: String,
+      required: true,
+    },
+    publisher: { // 발행처
+      type: String,
+      required: true,
+    },
+    published_date: { // 발행일
+      type: Date,
+      required: true,
+    },
+    price: { // 가격
+      type: Number,
+      required: true,
+    },
+    category: { // 분류
+      type : String,
+      required : true,
+      trim: true,
+    },
+    stock: { // 재고
+      type: Number,
+      defautlt : 10,
+      required: true,
+    },
+  },
+  {
+    timestamp: true,
+    collection: "product",
+  }
+  
+);
+
+// 문자열 형태의 "category" 매개변수를 받아 쉼표로 구분된 각 단어 앞에 "#"을 붙인 뒤 다시 공백으로 구분된 문자열로 변환하는 함수
+productSchema.static("formatHashtags", function (category) {
+  return category
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word : `#${word}`))
+    .join(" ");
+});
+
+
+
+module.exports = productSchema;
