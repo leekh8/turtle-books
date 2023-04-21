bb0b97a9cb767b54f6fc7ee04e1927d7255071a8
leekh <amysun125@gmail.com>
2023-04-21 22:43:43 +0900

    Featur: Update app.js

--- back-end/app.js
+++ back-end/app.js
@@ -3,7 +3,6 @@
 // import { productRouter } from './routes/product-router';
 // import { categoryRouter } from './routes/category-router';
 
-
 // const app = express();
 // app.use(cors());
 
@@ -20,3 +19,31 @@
 // });
 
 // exports.app = app;
+
+// --------------------------------
+
+// app.js
+
+/*
+express 설정, middleware 적용, 라우팅 설정, 서버 실행
+*/
+
+const express = require("express");
+const cors = require("cors");
+
+// 라우터 연결
+const usersRouter = require("./routes/user-router"); // user(사용자) router 연결
+
+// 미들웨어 적용
+const app = express();
+app.use(express.json()); // post 요청에서 body 추출
+app.use(cors()); // cors로 다른 도메인의 요청 허용
+
+app.use(cors()); // cors 에러 방지
+app.use(express.json()); // content-type: application/json 형태
+app.use(express.urlencoded({ extended: false })); // content-type: application/x-www-form-urlencoded 형태
+
+// api 라우터
+app.use("/user", usersRouter);
+
+module.exports = app;
