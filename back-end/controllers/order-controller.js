c9cf2735b2321ed4d7c163eee07567140987d25f
[sw4/1반]김종현 <donslam1234@gmail.com>
2023-04-24 19:21:38 +0900

    0424 PM

--- back-end/controllers/order-controller.js
+++ back-end/controllers/order-controller.js
@@ -8,38 +8,30 @@ class OrderController {
       totalAmount,
       recipientName,
       recipientPhoneNumber,
+      cartItems,
     } = req.body;
-
-    productList = productList.split(",");
-    countList = countList.split(",");
 
     if (
       !buyer ||
-      !productList ||
-      !countList ||
       !shippingStatus ||
       !shippingPostCode ||
-      !shippingStreetAddress ||
       !totalAmount ||
       !recipientName ||
-      !recipientPhoneNumber
+      !recipientPhoneNumber ||
+      !cartItems
     ) {
       return res.status(400).json("입력 데이터 부족");
     }
 
     try {
-      const createdNewOrder = await orderService.addOrder({
+      const createdNewOrder = await Order.create({
         buyer,
-        productList,
-        countList,
         shippingStatus,
         shippingPostCode,
-        shippingStreetAddress,
-        shippingExtraAddress,
-        shippingRequestMessage,
         totalAmount,
         recipientName,
         recipientPhoneNumber,
+        cartItems,
       });
       return res.status(200).json(createdNewOrder);
     } catch (e) {
@@ -47,61 +39,6 @@ class OrderController {
     }
   }
 
-  async getOrderList(req, res, next) {
-    if (Object.keys(req.query).length === 0) {
-      const orderList = await orderService.getOrderList();
-      return res.status(200).json(orderList);
-    } else {
-      const { oid } = req.query;
-
-      if (!oid) {
-        return res.status(400).json("에러, 쿼리 스트링에 oid이 존재해야 함");
-      }
-
-      try {
-        const oidArr = oid.split(",");
-        const orderList = await orderService.getOrderList(oidArr);
-        return res.status(200).json(orderList);
-      } catch (e) {
-        next(e);
-      }
-    }
-  }
-
-  async getOrder(req, res, next) {
-    const { oid } = req.params;
-
-    if (!oid) {
-      return res.status(400).json("입력 값이 부족합니다.");
-    }
-
-    try {
-      const order = await orderService.getOrderById(oid);
-      return res.status(200).json(order);
-    } catch (e) {
-      next(e);
-    }
-  }
-
-  async editOrder(req, res, next) {
-    const { oid } = req.params;
-    let { buyer, productList, countList, totalAmount } = req.body;
-
-    if (buyer || productList || countList || totalAmount) {
-      return res
-        .status(400)
-        .json(
-          "주문 정보에 대해서 변경할 수 없는 값에 대해 변경을 시도하여 거절되었습니다."
-        );
-    }
-
-    try {
-      const updatedOrder = await orderService.editOrder(oid, req.body);
-      return res.status(200).json(updatedOrder);
-    } catch (e) {
-      next(e);
-    }
-  }
 
   async removeOrder(req, res, next) {
     const { oid } = req.params;
--- back-end/db/schemas/order-schema.js
+++ back-end/db/schemas/order-schema.js
@@ -3,47 +3,44 @@ const { Schema, model } = require('mongoose');
 const OrderSchema = new Schema(
   //user 스키마에서 유저 정보를 받아서 buyer로 저장.
   {
-  buyer: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  shippingStatus: {
-    type: String,
-    default: "배송준비중",
-  },
-  shippingPostCode: {
-    type: String,
-    required: true,
-  },
-  recipientName: {
-    type : String,
-    required : true,
-  },
-  recipientPhoneNumber: {
-    type : Number,
-    required : true,
-  },
-  totalAmount: {
-    type: String,
-    required: true,
-  },
-  // 장바구니세션에서 받아오기
-  cartItems : [{
-    title: {
-      type : String,
-      required : true,
+    buyer: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    price : {
+    shippingStatus: {
+      type: String,
+      default: "배송준비중",
+    },
+    shippingPostCode: {
+      type: String,
+      required: true,
+    },
+    totalAmount: {
       type: Number,
-      required : true,
+      required: true,
     },
-    stock : {
+    recipientName: {
+      type: String,
+      required: true,
+    },
+    recipientPhoneNumber: {
       type: Number,
-      required : true,
+      required: true,
     },
-  }]
-},
+    // 장바구니세션에서 받아오기
+    cartItems: [{
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true,
+      },
+      quantity : {
+        type : Number,
+        required: true,
+      }
+    }]
+  },
   {
     timestamps: true,
     collection: "orders",
