2a5ddcf6de4a5e01229851f43bc59b9b06ef134e
JoSooha <dlrhdcjs@naver.com>
2023-04-25 18:40:26 +0900

    feat: itemdetail에서 장바구니 누르면 localstorage set

--- front-end/views/Itemdetail/index.js
+++ front-end/views/Itemdetail/index.js
@@ -24,6 +24,8 @@ document.addEventListener("DOMContentLoaded", function () {
     plusBtn.addEventListener("click", plusNum);
   });
 
+
+
   //장바구니 버튼 alert
   function showalert() {
     const result = confirm("장바구니에 담겼습니다.\n장바구니로 이동하시겠습니까?");
@@ -35,5 +37,18 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
+  //담은거 로컬스토리지에 넣기
+  function pushlocal() {
+    // const item = document.querySelector() //book을 식별가능한 id를 넣고 장바구니 렌더링 할 때 id로 db를 찾아서 다른 정보들을 가져와야할까? 
+    const item = "id1"
+    const cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+    cartItems.push(item);
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }
+
   const cartbutton = document.querySelector(".add-to-cart-btn")
   cartbutton.addEventListener("click", showalert);
+  cartbutton.addEventListener("click", pushlocal);
+
+
+  
