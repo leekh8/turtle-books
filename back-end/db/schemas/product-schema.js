465c4d63129edb5386935f4da90d76148f6a051f
[SW4/2반] 권희경 <mkmc020@gmail.com>
2023-04-26 19:59:27 +0900

    변경사항 없음

--- back-end/db/schemas/product-schema.js
+++ back-end/db/schemas/product-schema.js
@@ -1,50 +1,58 @@
+const { Schema } = require("mongoose");
 
-const { Schema } = require('mongoose');
-
-const productSchema = new Schema (
+const productSchema = new Schema(
   {
-    title: { // 제목
+    title: {
+      // 제목
       type: String,
       required: true,
     },
-    author: { // 작가명
+    author: {
+      // 작가명
       type: String,
       required: true,
     },
-    publisher: { // 발행처
+    publisher: {
+      // 발행처
       type: String,
       required: true,
     },
-    publishDate: { // 발행일
+    publishDate: {
+      // 발행일
       type: Date,
       required: true,
     },
-    description: { // 책 소개
+    description: {
+      // 책 소개
       type: String,
       required: true,
       default: "책 소개 준비중",
     },
-    price: { // 가격
+    price: {
+      // 가격
       type: Number,
       required: true,
     },
-    topic: { // best, steady, new
-      type: String,
-      required: true, 
-    },
-    category: { // 분류
+    topic: {
+      // best, steady, new
       type: String,
       required: true,
     },
-    stock: { // 재고
+    category: {
+      // 분류
+      type: String,
+      required: true,
+    },
+    stock: {
+      // 재고
       type: Number,
       default: 10,
       required: true,
-    }
+    },
   },
   {
     timestamps: true,
-    collection: "products"
+    collection: "products",
   }
 );
 
