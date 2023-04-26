c71d2e895f61df02b0217ee439f2b0758a5853c8
leekh <amysun125@gmail.com>
2023-04-26 16:26:48 +0900

    feat: npm 동작 확인

--- back-end/app.js
+++ back-end/app.js
@@ -1,27 +1,3 @@
-// import cors from 'cors';
-// import express from 'express';
-// import { productRouter } from './routes/product-router';
-// import { categoryRouter } from './routes/category-router';
-
-// const app = express();
-// app.use(cors());
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-
-// app.use("/product", productRouter);
-// app.use("/category", categoryRouter);
-
-// app.use(errorHandler);
-
-// app.listen(PORT, () => {
-//   console.log("정상적으로 서버를 시작하였습니다");
-// });
-
-// exports.app = app;
-
-// --------------------------------
-
 // app.js
 
 /*
@@ -33,8 +9,10 @@ const cors = require("cors");
 
 // 라우터 연결
 const usersRouter = require("./routes/user-router"); // user(사용자) router 연결
-const productRouter = require('./routes/product-router');
-const categoryRouter = require('./routes/category-router');
+// const router = require("./routes/order-list")
+const productRouter = require("./routes/product-router");
+const categoryRouter = require("./routes/category-router");
+const errorHandler = require("./middlewares/error-handler");
 
 // 미들웨어 적용
 const app = express();
@@ -42,19 +20,13 @@ app.use(express.json()); // post 요청에서 body 추출, content-type: applica
 app.use(cors()); // cors 에러 방지, cors로 다른 도메인의 요청 허용
 app.use(express.urlencoded({ extended: false })); // content-type: application/x-www-form-urlencoded 형태
 
-
 // api 라우터
 app.use("/api/user", usersRouter);
-app.use("/user/myroom/order-list", router); // order-list 연결
+// app.use("/user/myroom/order-list", router); // order-list 연결
 app.use("/api/product", productRouter);
 app.use("/api/category", categoryRouter);
 
 // 에러 처리
-app.use((err, req, res, next) => {
-  console.log(err.stack);
-  res.status(err.statusCode || 500).json({ message: `server error occured` });
-});
+app.use(errorHandler);
 
 module.exports = app;
-
-// module.exports = app;
