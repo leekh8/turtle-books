9f3e703d29da93a0f6ac0f869449585fb6b3a902
leekh <amysun125@gmail.com>
2023-04-27 14:44:51 +0900

    feature: update order router

--- back-end/routes/order-router.js
+++ back-end/routes/order-router.js
@@ -1,23 +1,13 @@
-const { Router } = require('express');
-const router = Router();
-const orderController = require('../controllers/order-controller');
-const {bodyChecker} = require('../middlewares/body-checker');
+const { Router } = require("express");
+const orderRouter = Router();
+const orderController = require("../controllers/order-controller");
+const { bodyChecker } = require("../middlewares/body-checker");
 
+orderRouter.get("/", orderController.pageRender); // 주문 페이지 렌더링
+orderRouter.post("/", orderController.addOrder); //주문 생성
+orderRouter.get("/", orderController.getOrderList); //주문 목록 가져오기
+orderRouter.get("/:oid", orderController.getOrder); // 특정 주문 번호에 대한 주문 정보 가져오기
+orderRouter.put("/:oid", bodyChecker, orderController.editOrder); // 주문 수정
+orderRouter.delete("/:oid", orderController.removeOrder); // 주문 삭제
 
-
-
-router.get("/", orderController.pageRender); // 주문 페이지 렌더링
-
-router.post("/", orderController.addOrder); //주문 생성
-router.get("/", orderController.getOrderList); //주문 목록 가져오기
-router.get("/:oid", orderController.getOrder); // 특정 주문 번호에 대한 주문 정보 가져오기
-router.put("/:oid", bodyChecker, orderController.editOrder); // 주문 수정
-router.delete("/:oid", orderController.removeOrder); // 주문 삭제
-
-
-
-
-
-
-
-module.exports = router;
+module.exports = orderRouter;
