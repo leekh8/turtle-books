fc41717488df19f7115264a7136c017abd91ef9e
young jo <a10019610@gmail.com>
2023-04-27 21:49:39 +0900

    [FE] viewRouter 추가 및 폴더 명 수정

--- front-end/view-router.js
+++ front-end/view-router.js
@@ -4,6 +4,16 @@ import path from "path";
 const viewsRouter = express.Router();
 //home
 viewsRouter.use("/", serveStatic("home")); // 파일명과 html 폴더명 일치해야함
+viewsRouter.use("/admin", serveStatic("admin"));
+viewsRouter.use("/cart", serveStatic("cart"));
+viewsRouter.use("/itemDetail", serveStatic("itemDetail"));
+viewsRouter.use("/itemList", serveStatic("itemList"));
+viewsRouter.use("/login", serveStatic("login"));
+viewsRouter.use("/mypage", serveStatic("mypage"));
+viewsRouter.use("/orderCheck", serveStatic("orderCheck"));
+viewsRouter.use("/orderComplete", serveStatic("orderComplete"));
+viewsRouter.use("/order", serveStatic("order"));
+viewsRouter.use("/signup", serveStatic("signup"));
 
 //html 연결해주는 매서드
 function serveStatic(resource) {
