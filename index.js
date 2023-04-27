b83cffc2f2bd2e72d5a7a0e9993032faa8e29846
leekh <amysun125@gmail.com>
2023-04-27 13:00:30 +0900

    update

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-const cors = require('cors');
-const express = require('express');
-const productRouter = require('./back-end/routes/product-router')
-const categoryRouter = require('./back-end/routes/category-router');
-// const app = require("./back-end/app.js");
+// const express = require("express");
+// const cors = require("cors");
+
+// const productRouter = require("./back-end/routes/product-router");
+// const categoryRouter = require("./back-end/routes/category-router");
+// const userRouter = require("./back-end/routes/user-router");
+const app = require("./back-end/app.js");
+
 const PORT = 3000;
-// 
-const app = express();
-app.use(cors());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// const app = express();
 
-app.use("/product", productRouter);
-app.use("/category", categoryRouter);
+// app.use(cors());
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+
+// app.use("/product", productRouter);
+// app.use("/category", categoryRouter);
 
 // app.use(errorHandler);
 
 app.listen(PORT, () => {
-  console.log("정상적으로 서버를 시작하였습니다");
+  console.log(`정상적으로 서버를 시작하였습니다. PORT: ${PORT}`);
 });
