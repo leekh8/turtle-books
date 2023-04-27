1841651c24b6a0705905cae62eab31b64738b619
leekh <amysun125@gmail.com>
2023-04-27 13:23:11 +0900

    update app.js index.js

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
-const cors = require("cors");
-const express = require("express");
-const productRouter = require("./back-end/routes/product-router");
-const categoryRouter = require("./back-end/routes/category-router");
-// const app = require("./back-end/app.js");
+const app = require("./back-end/app.js");
 const PORT = 3000;
-//
-const app = express();
-app.use(cors());
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use("/product", productRouter);
-app.use("/category", categoryRouter);
-
-// app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log("3000 포트 서버 가동 시작");
