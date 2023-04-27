8b8c0e767253d9a119dd6d7e9252face666db8d9
KOJIYEON <kamjoo990406@gmail.com>
2023-04-27 13:04:14 +0900

    rename:index삭제,app,index수정정

--- back-end/app.js
+++ back-end/app.js
@@ -1,46 +1,11 @@
-// import cors from 'cors';
-// import express from 'express';
-// import { productRouter } from './routes/product-router';
-// import { categoryRouter } from './routes/category-router';
-
-// const app = express();
-// app.use(cors());
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-
-// app.use("/product", productRouter);
-// app.use("/category", categoryRouter);
-
-// app.use(errorHandler);
-
-// app.listen(PORT, () => {
-//   console.log("정상적으로 서버를 시작하였습니다");
-// });
-
-// exports.app = app;
-
-// --------------------------------
-
-// app.js
-
-/*
-express 설정, middleware 적용, 라우팅 설정, 서버 실행
-*/
-
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => console.log('3000 포트, 서버 가동'));
 
 app.use(cors()); // cors 에러 방지
 app.use(express.json()); // content-type: application/json 형태
 app.use(express.urlencoded({ extended: false })); // content-type: application/x-www-form-urlencoded 형태
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // 라우터 연결
@@ -57,4 +22,4 @@ app.use("/admin", adminRouter);
 app.use("/api/product", productRouter);
 app.use("/api/category", categoryRouter);
 
-// module.exports = app;
+module.exports = app;
